a62bb092214d2db444a22714db58f08c0022c2f1
HuangYi <123337671@qq.com>
2021-01-08 11:57:48 +0800

    A001完成

---
+++ src/HuangYi/A001AaddB/Main.java
@@ -0,0 +1,12 @@
+package HuangYi.A001AaddB;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		Integer a = input.nextInt();
+		Integer b = input.nextInt();
+		System.out.print(a + b);
+	}
+}
+
--- src/HuangYi/A001test/Main.java
+++
@@ -1,8 +0,0 @@
-package HuangYi.A001test;
-
-public class Main {
-	public static void main(String args[]) {
-		System.out.print("Hello World");
-	}
-}
-
