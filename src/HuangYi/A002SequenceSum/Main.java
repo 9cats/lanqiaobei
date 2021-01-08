435305947486fee4257f340419540423217fe925
HuangYi <123337671@qq.com>
2021-01-08 12:07:52 +0800

    A002完成

---
+++ src/HuangYi/A002SequenceSum/Main.java
@@ -0,0 +1,10 @@
+package HuangYi.A002SequenceSum;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		Long num = new Scanner(System.in).nextLong();
+		System.out.print((1+num)*num/2);
+	}
+}
